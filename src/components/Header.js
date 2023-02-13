b2c1a74a9f58a36c6f862fa8c37038500d7399c9
ice5000 <95354975+ice5000@users.noreply.github.com>
2023-02-14 00:25:06 +0100

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="App-header">
+    <div>origin</div>
+    <div> 
+    <input className="search" type="text" placeholder="Search.."></input>
+     <button type="submit"><i class="fa fa-search"></i></button>
+    
+    Explore Stats <button>Create</button> Avatar </div>
+  </header>
+  )
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="main">
+    <div>
+          <h1>LOREM IPSUM</h1>
+        <p>Dorem ipsum dolor sit amet, consectetur adipiscing elit. Nun vulputate <br/>
+        libero et velit interdum, ac aliquet odio mattis. Class aptent
+        taciti sociosqu<br/> 
+        ad litora torquent per conubia nostra, per inceptos himenaeos.
+      </p>
+    </div>
+    <div>Image</div>
+    </div>
+  )
+}
+
+export default Main
